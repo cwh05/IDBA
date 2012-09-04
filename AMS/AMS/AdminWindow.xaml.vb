6fa733b1480a8a7fa6bd3a8a9ead5160848f30a9
Pongsakorn Paetrakul <pongsakorn.pa@gmail.com>
2012-09-05 03:21:57 +1000

    Intialize the AMS solution

---
+++ AMS/AMS/AdminWindow.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports MahApps.Metro.Controls
+
+Public Class AdminWindow : Inherits MetroWindow
+
+    Private Sub btnCreateDepartment_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        tabContent.SelectedIndex = 0
+    End Sub
+
+    Private Sub btnCreateProgram_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        tabContent.SelectedIndex = 1
+    End Sub
+
+    Private Sub btnCreateAccount_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        tabContent.SelectedIndex = 2
+    End Sub
+End Class
---
+++ AMS/AMS/LoginWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Imports MahApps.Metro.Controls
+
+Class LoginWindow : Inherits MetroWindow
+
+    Private Sub btnLogin_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        Dim adminWindow As New AdminWindow()
+        adminWindow.Visibility = Windows.Visibility.Visible
+    End Sub
+End Class
