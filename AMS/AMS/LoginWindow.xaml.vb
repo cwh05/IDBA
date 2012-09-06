6163435de79d96f64dcbe88f30d2031daed4fefc
Wai Hoong <cwh05@hotmail.com>
2012-09-06 13:11:29 +1000

    new LoginWindow...

--- AMS/AMS/LoginWindow.xaml.vb
+++ AMS/AMS/LoginWindow.xaml.vb
@@ -2,8 +2,24 @@
 
 Class LoginWindow : Inherits MetroWindow
 
-    Private Sub btnLogin_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
-        Dim adminWindow As New AdminWindow()
-        adminWindow.Visibility = Windows.Visibility.Visible
+    Private Sub btnLogin_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnLogin.Click
+        'validate input textboxes
+        If txtUsername.Text.Length = 0 And txtPassword.Password.Length = 0 Then
+            errorMsg.Visibility = Windows.Visibility.Visible
+            txtUsername.Focus()
+
+        Else
+            'connect to database
+
+
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub MetroWindow_Loaded(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+        txtUsername.Focus()
     End Sub
 End Class
