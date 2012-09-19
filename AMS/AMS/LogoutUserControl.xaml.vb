2c692a98a8eda6c79129f643c63029780b3f6cb6
Wai Hoong <cwh05@hotmail.com>
2012-09-19 17:07:39 +1000

    added logout user control
    added logout button to StudentWindow

---
+++ AMS/AMS/LogoutUserControl.xaml.vb
@@ -0,0 +1,18 @@
+﻿Public Class LogoutUserControl
+
+    Private Sub btnLogout_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnLogout.Click
+        Dim window As New LoginWindow()
+
+        'close all active windows
+        For Each i As Window In Application.Current.Windows
+            If Not i.Title.Equals("Academic Management System") Then
+                i.Close()
+            End If
+        Next
+
+        GC.Collect()
+
+        'launch login window
+        window.Show()
+    End Sub
+End Class
