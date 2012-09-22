4fff84aba81dac0e00383f272bfb8b72eb9f3eb5
Edward Sia <edwardskt@hotmail.com>
2012-09-22 19:43:43 +1000

    StaffWindows updates

--- AMS/AMS/Controllers/StaffWindowController.vb
+++ AMS/AMS/Controllers/StaffWindowController.vb
@@ -2,8 +2,9 @@
     Private db As New AMSEntities()
 
     ''' <summary>
-    ''' This function retrives all courses data
+    ''' This function retrives all courses data by staffID
     ''' </summary>
+    ''' <param name="staffID">A staff ID</param>
     ''' <returns>A list of all course objects</returns>
     ''' <remarks></remarks>
     Public Function GetAllCourseOfStaff(ByRef staffID As Int32) As List(Of Course)
@@ -28,5 +29,63 @@
         Catch ex As Exception
         End Try
         Return Nothing
+    End Function
+
+    ''' <summary>
+    ''' This function retrieves a student enrollment records by Course taught by the logged in lecturer
+    ''' </summary>
+    ''' <param name="courseID">A course ID</param>
+    ''' <returns>A list of enrollment courses</returns>
+    ''' <remarks></remarks>
+    Public Function GetStudentEnrollmentByCourseID(ByRef courseID As Int32) As List(Of Enrollment)
+        Try
+            Dim enrollmentList As New List(Of Enrollment)
+            For Each i In db.GetStudentEnrollmentByCourseID(courseID)
+                Dim enrollment As New Enrollment
+
+                'assign values to enrollment object
+                enrollment.StudentID = i.StudentID
+                enrollment.StudentFirstName = i.StudentFirstName
+                enrollment.StudentLastName = i.StudentLastName
+                enrollment.Email = i.Email
+                enrollment.CourseID = i.CourseID
+                enrollment.CourseName = i.CourseName
+                enrollment.CourseCode = i.CourseCode
+                enrollment.Marks = i.Marks
+
+                enrollmentList.Add(enrollment)
+            Next
+            Return enrollmentList
+
+        Catch ex As Exception
+        End Try
+
+        Return Nothing
+    End Function
+
+    ''' <summary>
+    ''' This function updates course details
+    ''' </summary>
+    ''' <param name="CourseCode">A course code string</param>
+    ''' <param name="CourseName">A course name string</param>
+    ''' <param name="CourseDescription">A course description string</param>
+    ''' <param name="CourseID">A course id</param>
+    ''' <returns>True if updated successfully or false if it is failed</returns>
+    ''' <remarks></remarks>
+    Public Function UpdateCourse(ByRef CourseCode As String, ByRef CourseName As String, ByRef CourseDescription As String,
+                                ByRef CourseID As Integer) As Boolean
+        'update 2 statements in a transaction
+
+        Try
+
+            db.UpdateCourse(CourseID, CourseName, CourseCode, CourseDescription)
+            db.SaveChanges()
+
+            Return True
+
+        Catch ex As Exception
+        End Try
+
+        Return False
     End Function
 End Class
--- AMS/AMS/StaffWindow.xaml.vb
+++ AMS/AMS/StaffWindow.xaml.vb
@@ -3,6 +3,8 @@
     Private controller As StaffWindowController = New StaffWindowController
     Private courseList As List(Of Course)
     Private ReadOnly StaffUsername As String
+    Private enrollCourseList As List(Of Enrollment) = New List(Of Enrollment)
+    Private editable As Boolean
 
     Public Sub New(ByRef username As String)
         ' This call is required by the designer.
@@ -10,6 +12,8 @@
 
         ' Add any initialization after the InitializeComponent() call.
         StaffUsername = username
+        editable = False
+
     End Sub
 
     Private Sub btnMenu_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
@@ -21,23 +25,90 @@
                 tabContent.SelectedIndex = 2
             Case "btnViewCourseEnrolment"
                 tabContent.SelectedIndex = 3
-                btnViewCourseEnrolment_Click()
+                ViewCourseEnrolment(viewCourseListboxControl)
             Case "btnCourseDetail"
                 tabContent.SelectedIndex = 4
+                ViewCourseEnrolment(courseDetailListboxControl)
         End Select
     End Sub
 
-    Private Sub btnViewCourseEnrolment_Click()
+    Private Sub ViewCourseEnrolment(listboxControl As ListBox)
 
         Try
             'retrieve all courses record
             courseList = controller.GetAllCourseOfStaff(CUInt(StaffUsername))
-            staffListboxControl.ItemsSource = courseList
+            listboxControl.ItemsSource = courseList
 
-            staffListboxControl.Items.Refresh()
+            listboxControl.Items.Refresh()
 
         Catch ex As Exception
         End Try
+
+    End Sub
+
+    Private Sub viewCourseListboxControl_SelectionChanged(sender As System.Object, e As System.Windows.Controls.SelectionChangedEventArgs) Handles viewCourseListboxControl.SelectionChanged
+
+        Try
+            'retrieve all courses record
+            If viewCourseListboxControl.SelectedIndex >= 0 Then
+                enrollCourseList = controller.GetStudentEnrollmentByCourseID(CUInt(viewCourseListboxControl.SelectedValue.ToString))
+                studentListboxControl.ItemsSource = enrollCourseList
+
+                studentListboxControl.Items.Refresh()
+            End If
+            
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Exception")
+        End Try
+    End Sub
+
+    Private Sub courseDetailListboxControl_SelectionChanged(sender As System.Object, e As System.Windows.Controls.SelectionChangedEventArgs) Handles courseDetailListboxControl.SelectionChanged
+
+        Try
+            If courseDetailListboxControl.SelectedIndex >= 0 Then
+                Dim ent As New Course
+                ent = courseDetailListboxControl.SelectedItem
+
+                tbCourseCode.Text = ent.CourseCode
+                tbCourseName.Text = ent.CourseName
+                tbCourseDesc.Text = ent.CourseDescription
+
+                tbCourseCode.IsEnabled = False
+                tbCourseName.IsEnabled = False
+                tbCourseDesc.IsEnabled = False
+                editable = False
+                btnEdit.Content = "edit"
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Exception")
+        End Try
+    End Sub
+
+    Private Sub btnEdit_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnEdit.Click
+
+        If editable Then
+            If tbCourseCode.Text.Length > 0 And tbCourseDesc.Text.Length > 0 And tbCourseName.Text.Length > 0 Then
+
+                If MsgBox("Update course?", MsgBoxStyle.YesNo, "Confirmation") = MsgBoxResult.Yes Then
+                    If controller.UpdateCourse(tbCourseCode.Text, tbCourseName.Text, tbCourseDesc.Text, courseDetailListboxControl.SelectedValue) Then
+                        tbCourseCode.IsEnabled = False
+                        tbCourseName.IsEnabled = False
+                        tbCourseDesc.IsEnabled = False
+                        editable = False
+                        btnEdit.Content = "edit"
+                        ViewCourseEnrolment(courseDetailListboxControl)
+                    End If
+                End If
+            End If
+        Else
+            tbCourseCode.IsEnabled = True
+            tbCourseName.IsEnabled = True
+            tbCourseDesc.IsEnabled = True
+            editable = True
+            btnEdit.Content = "update"
+        End If
+
 
     End Sub
 End Class
